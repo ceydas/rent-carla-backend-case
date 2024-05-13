774bf187def3d246649d414aba5bcbdd36869198
Ceyda Sahin <ceydasahin889@gmail.com>
2024-05-13 18:39:18 +0300

    GET by id
    Added get by id function.

--- rent-carla-backend-case/src/main/java/com/ceydog/rentcarlabackendcase/compensation/controller/CompensationController.java
+++ rent-carla-backend-case/src/main/java/com/ceydog/rentcarlabackendcase/compensation/controller/CompensationController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -55,6 +52,15 @@ public class CompensationController {
 
         List<CompensationSparseDto> compensationSparseDto = compensationService.getJobTitleAndSalary(pageable);
         return ResponseEntity.ok(compensationSparseDto);
+    }
+
+    @GetMapping("/id={id}")
+    public ResponseEntity<CompensationJsonDto> findById(@PathVariable Long id){
+
+        CompensationJsonDto compensationJsonDto = compensationService.findById(id);
+
+        return ResponseEntity.ok(compensationJsonDto);
+
     }
 
 
